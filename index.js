552d0d4c174100af3c0baeac90ff8c8b104d0b7c
Hassan Aguezoum <haguezoum@gmail.com>
2024-02-10 18:11:25 +0000

    init

---
+++ index.js
@@ -0,0 +1,45 @@
+// ==UserScript==
+// @name         Bus Script
+// @namespace    http://tampermonkey.net/
+// @version      2024-02-08
+// @description  try to take over the world!
+// @author       Hassan Aguezoum
+// @match        https://bus-med.1337.ma/
+// @icon         https://source.leet.ma/favicon.ico
+// @grant        none
+// @require      https://unpkg.com/ml5@latest/dist/ml5.min.js
+// ==/UserScript==
+
+'use strict';
+
+const csrfmiddlewaretoken = document.getElementsByName("csrfmiddlewaretoken")[0].value;
+const traget_id = document.getElementsByName("traget_id")[0].value;
+const captcha_0 = document.getElementsByName("captcha_0")[0].value;
+const captcha_1 = document.getElementsByName("captcha_1")[0].value;
+
+function fetchMe()
+{
+    fetch("https://bus-med.1337.ma/create-reservation", {
+        "headers": {
+            "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+            "accept-language": "en-US,en;q=0.9",
+            "cache-control": "max-age=0",
+            "content-type": "application/x-www-form-urlencoded",
+            "sec-ch-ua": "\"Not/A)Brand\";v=\"99\", \"Google Chrome\";v=\"115\", \"Chromium\";v=\"115\"",
+            "sec-ch-ua-mobile": "?0",
+            "sec-ch-ua-platform": "\"macOS\"",
+            "sec-fetch-dest": "document",
+            "sec-fetch-mode": "navigate",
+            "sec-fetch-site": "same-origin",
+            "sec-fetch-user": "?1",
+            "upgrade-insecure-requests": "1",
+            "cookie": "login_state=65YL5J1RGPLL878AJX0PMM4; csrftoken=fAypqVB0SAvholQTl2jjFKOyJ0dQuQyQ; sessionid=vqxgj6amzjow5wxk9pfq5pzyjszzg28y",
+            "Referer": "https://bus-med.1337.ma/",
+            "Referrer-Policy": "same-origin"
+        },
+        "body": "csrfmiddlewaretoken=${csrfmiddlewaretoken}&traget_id=${traget_id}&captcha_0=${captcha_0}4&captcha_1=${captcha_1}",
+        "method": "POST"
+    });
+}
+
+fetchMe();
