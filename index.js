c10b41d0e788493442836be6d6220480feb3cf04
Hassan Aguezoum <haguezoum@gmail.com>
2024-02-10 20:55:49 +0000

    update

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name         Bus Script
+// @name         Akhtaf n Tmazirt
 // @namespace    http://tampermonkey.net/
 // @version      2024-02-08
 // @description  try to take over the world!
@@ -7,39 +7,52 @@
 // @match        https://bus-med.1337.ma/
 // @icon         https://source.leet.ma/favicon.ico
 // @grant        none
-// @require      https://unpkg.com/ml5@latest/dist/ml5.min.js
 // ==/UserScript==
 
 'use strict';
 
 const csrfmiddlewaretoken = document.getElementsByName("csrfmiddlewaretoken")[0].value;
-const traget_id = document.getElementsByName("traget_id")[0].value;
+const traget_id = document.querySelector("body > div.maincontainer > div > div.card.available > div.interact_container > button:nth-child(1)").attributes[3].value;
 const captcha_0 = document.getElementsByName("captcha_0")[0].value;
-const captcha_1 = document.getElementsByName("captcha_1")[0].value;
+let captcha_1 ;
+
+const reservBtn = document.querySelector("#note_form > div.modal-body > img");
+
+reservBtn.addEventListener("click", () => {
+    captcha_1 = prompt("Please enter: ");
+    console.log(" > " + captcha_1);
+    console.log(" > " + csrfmiddlewaretoken);
+    console.log(" > " + traget_id);
+    fetchMe();
+});
 
 function fetchMe()
 {
-    fetch("https://bus-med.1337.ma/create-reservation", {
-        "headers": {
-            "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
-            "accept-language": "en-US,en;q=0.9",
-            "cache-control": "max-age=0",
-            "content-type": "application/x-www-form-urlencoded",
-            "sec-ch-ua": "\"Not/A)Brand\";v=\"99\", \"Google Chrome\";v=\"115\", \"Chromium\";v=\"115\"",
-            "sec-ch-ua-mobile": "?0",
-            "sec-ch-ua-platform": "\"macOS\"",
-            "sec-fetch-dest": "document",
-            "sec-fetch-mode": "navigate",
-            "sec-fetch-site": "same-origin",
-            "sec-fetch-user": "?1",
-            "upgrade-insecure-requests": "1",
-            "cookie": "login_state=65YL5J1RGPLL878AJX0PMM4; csrftoken=fAypqVB0SAvholQTl2jjFKOyJ0dQuQyQ; sessionid=vqxgj6amzjow5wxk9pfq5pzyjszzg28y",
-            "Referer": "https://bus-med.1337.ma/",
-            "Referrer-Policy": "same-origin"
-        },
-        "body": "csrfmiddlewaretoken=${csrfmiddlewaretoken}&traget_id=${traget_id}&captcha_0=${captcha_0}4&captcha_1=${captcha_1}",
-        "method": "POST"
-    });
+    console.log("fetching");
+   setTimeout(() => {
+        fetch("https://bus-med.1337.ma/create-reservation", {
+            "headers": {
+                "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+                "accept-language": "en-US,en;q=0.9",
+                "cache-control": "max-age=0",
+                "content-type": "application/x-www-form-urlencoded",
+                "sec-ch-ua": "\"Not/A)Brand\";v=\"99\", \"Google Chrome\";v=\"115\", \"Chromium\";v=\"115\"",
+                "sec-ch-ua-mobile": "?0",
+                "sec-ch-ua-platform": "\"macOS\"",
+                "sec-fetch-dest": "document",
+                "sec-fetch-mode": "navigate",
+                "sec-fetch-site": "same-origin",
+                "sec-fetch-user": "?1",
+                "upgrade-insecure-requests": "1",
+                "cookie": "login_state=65YL5J1RGPLL878AJX0PMM4; csrftoken=fAypqVB0SAvholQTl2jjFKOyJ0dQuQyQ; sessionid=vqxgj6amzjow5wxk9pfq5pzyjszzg28y",
+                "Referer": "https://bus-med.1337.ma/",
+                "Referrer-Policy": "same-origin"
+            },
+            "body": `csrfmiddlewaretoken=${csrfmiddlewaretoken}&traget_id=${traget_id}&captcha_0=${captcha_0}4&captcha_1=${captcha_1}`,
+            "method": "POST"
+        });
+        console.log("Done");
+   }, 1000);
 }
 
-fetchMe();
+console.log("Script is running");
