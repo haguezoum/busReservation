41ab1084a03a14fbd29b73ccdb1c825c6c5f758e
Hassan Aguezoum <haguezoum@gmail.com>
2024-02-27 14:24:10 +0000

    setup the bot

--- index.js
+++ index.js
@@ -1,98 +1,87 @@
-// ==UserScript==
-// @name         Akhtaf n Tmazirt
-// @namespace    http://tampermonkey.net/
-// @version      2024-02-08
-// @description  try to take over the world!
-// @author       Hassan Aguezoum
-// @match        https://bus-med.1337.ma/
-// @icon         https://source.leet.ma/favicon.ico
-// @grant        none
-// ==/UserScript==
 
-'use strict';
 
-const csrfmiddlewaretoken = document.getElementsByName("csrfmiddlewaretoken")[0].value;
-const button_target = document.querySelector("body > div.maincontainer > div > div.card.available > div.interact_container > button:nth-child(1)");
+const { Telegraf, Input } = require('telegraf');
+const { message } = require('telegraf/filters')
+const cheerio = require('cheerio');
+const fetch = require('node-fetch');
+
+const cookies = 'login_state=5RDUM953SFFF95M7G5WDH1Q; csrftoken=WgujQBJ9y9bi5cYWyGDu3KueMa7aVaXv; sessionid=543tbr724aw5dro62z4lvzhucxfindj0';
+const bot = new Telegraf('5602884622:AAF7mQnSWlasSB0FWYPmhjy9YHkcRpaDznk');
+let csrfmiddlewaretoken;
+let captcha_0;
+let reservBtn;
 let traget_id;
-const captcha_0 = document.getElementsByName("captcha_0")[0].value;
-const reservBtn = document.querySelector("#note_form > div.modal-body > img");
 let captcha_1;
 let captchaTime;
 let time;
 let timeToRun = new Date();
-let divs = document.querySelectorAll('.card:not(.locked)');
 let timeNow = new Date();
+let img_url;
 
-for (let i = 0; i < divs.length; i++) {
-    console.log(divs[i].querySelector(".place").innerText.includes("Martil", "Ahriq"));
-    if (divs[i].querySelector(".place").innerText.includes("Martil", "Ahriq")) {
-        traget_id = divs[i].querySelector("div.card > div.interact_container > button:nth-child(1)") ?
-            divs[i].querySelector("div.card > div.interact_container > button:nth-child(1)").attributes[3].value : null;
-    }
-    else {
-        traget_id = null;
-    }
-}
-// get ticket --------->
+// test bot --------->
+bot.start((ctx) => ctx.reply('Welcome to Hassan Bot!'));
+// bot.command(("lol"), ctx => { ctx.reply("Please enter: "); });
 
-fetch("https://bus-med.1337.ma/").then((res) => { return res.text() }).then(data => {
-    let domparser = new DOMParser();
-    let html = domparser.parseFromString(data, "text/html");
-    return (html);
-}).then(htmlTree => {
-    let img = htmlTree.querySelector("#note_form > div.modal-body > img");
-    console.log(img.src);
-}).catch(err => {
-    console.error(err);
+// -------------------------------------------------------------------------->
+// 4 = 30;
+bot.command("captcha", async (ctx) => {
+    const photoURL = await fetchImage(cookies);
+    await ctx.replyWithPhoto(Input.fromURL(photoURL, "photo.png"));
+});
+// -------------------------------------------------------------------------->
+bot.on("text", (ctx) => {
+    const text = ctx.message.text.split(":")[0];
+    time = ctx.message.text.split(":")[1] ? ctx.message.text.split(":")[1] : timeNow.getHours();
+
+    let interval = setInterval(() => {
+        if (time == timeToRun.getHours()) {
+            clearInterval(interval);
+            fetchMe(text);// get ticket
+            ctx.reply("Done ✅!");
+        }
+    }, 10);
 });
 
-// get ticket --------->
-
-reservBtn.addEventListener("click", () => {
-    captchaTime = prompt("Please enter: ");
-    console.log(captchaTime);
-    captcha_1 = captchaTime.split(":")[0];
-    time = captchaTime.split(":")[1] ? captchaTime.split(":")[1] : timeNow.getHours();
-
-});
-
-let interval = setInterval(() => {
-    if (time == timeToRun.getHours()) {
-        clearInterval(interval);
-        fetchMe();
-    }
-}, 10);
-
-function fetchMe() {
-
-    console.log("fetching");
-    console.log(`target id ${traget_id} | token ${csrfmiddlewaretoken} | captcha0 ${captcha_0} | captcha1 ${captcha_1} | time : ${time}`);
-    setTimeout(() => {
-        fetch("https://bus-med.1337.ma/create-reservation", {
-            "headers": {
-                "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
-                "accept-language": "en-US,en;q=0.9",
-                "cache-control": "max-age=0",
-                "content-type": "application/x-www-form-urlencoded",
-                "sec-ch-ua": "\"Not/A)Brand\";v=\"99\", \"Google Chrome\";v=\"115\", \"Chromium\";v=\"115\"",
-                "sec-ch-ua-mobile": "?0",
-                "sec-ch-ua-platform": "\"macOS\"",
-                "sec-fetch-dest": "document",
-                "sec-fetch-mode": "navigate",
-                "sec-fetch-site": "same-origin",
-                "sec-fetch-user": "?1",
-                "upgrade-insecure-requests": "1",
-                "cookie": "login_state=65YL5J1RGPLL878AJX0PMM4; csrftoken=fAypqVB0SAvholQTl2jjFKOyJ0dQuQyQ; sessionid=vqxgj6amzjow5wxk9pfq5pzyjszzg28y",
-                "Referer": "https://bus-med.1337.ma/",
-                "Referrer-Policy": "same-origin"
-            },
-            "body": `csrfmiddlewaretoken=${csrfmiddlewaretoken}&traget_id=${traget_id}&captcha_0=${captcha_0}&captcha_1=${captcha_1}`,
-            "method": "POST"
-        }).then(res => {
-            console.info(res);
+async function fetchImage(cookies) {
+    try {
+        const response = await fetch("https://bus-med.1337.ma/", {
+            method: "GET",
+            headers: {
+                Cookie: cookies
+            }
         });
-        console.log("Done");
-    }, 1000);
+
+        const data = await response.text();
+        const $ = cheerio.load(data);
+
+        csrfmiddlewaretoken = $("input[name='csrfmiddlewaretoken']").attr("value");
+        captcha_0 = $("input[name='captcha_0']").attr("value");
+        const img_url = $("img").attr("src");
+
+        console.log(`with Cheerio :: srfmiddlewaretoken: ${csrfmiddlewaretoken} | captcha_0: ${captcha_0} | captcha_1: ${captcha_1} | img_utl: ${img_url}`);
+
+        return (`https://bus-med.1337.ma/${img_url}`);
+    } catch (error) {
+        console.error("Error:", error);
+    }
 }
 
+function fetchMe(text) {
+    fetch("https://bus-med.1337.ma/", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+            Cookie: cookies
+        },
+        "body": `csrfmiddlewaretoken=${csrfmiddlewaretoken}&traget_id=30&captcha_0=${captcha_0}&captcha_1=${text}`,
+    })
+        .then((res) => {
+            return res;
+        }
+        )
+        .then(() => {
+            console.log("Done from server ...");
+        });
+}
+bot.launch();
 console.log("Script is running ...");
