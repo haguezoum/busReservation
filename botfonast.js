e98a686664ba95335b5556acbdbedba40874a6b1
Hassan Aguezoum <haguezoum@gmail.com>
2024-03-01 11:31:00 +0000

    ready to take taxi

--- botfonast.js
+++ botfonast.js
@@ -6,7 +6,7 @@
 /*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/29 09:48:38 by codespace         #+#    #+#             */
-/*   Updated: 2024/02/29 13:59:02 by codespace        ###   ########.fr       */
+/*   Updated: 2024/03/01 11:30:45 by codespace        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,16 +22,25 @@ let cookies = 'login_state=5RDUM953SFFF95M7G5WDH1Q; csrftoken=WgujQBJ9y9bi5cYWyG
 let csrfmiddlewaretoken;
 let captcha_0;
 let time = new Date();
-
+const timeId =
+{
+    "11": 9,
+    "12": 21,
+    "13": 52,
+    "14": 50,
+    "16": 30,
+    "18": 33,
+    "19": 40,
+    "20": 41,
+    "21": 29,
+    "22": 17,
+    "23": 38
+}
 
 async function fetchImage() {
-    console.log("fetching image ...");
     try {
         const response = await fetch("https://bus-med.1337.ma/", {
             method: "GET",
-            headers: {
-                Cookie: cookies
-            }
         });
 
         const html = await response.text();
@@ -69,14 +78,14 @@ function placeInput() {
     input.classList.add("takcholt");
     input.style.width = "150px";
     input.style.position = "absolute";
-    image.style.zIndex = "9999";
+    input.style.zIndex = "9999";
     input.style.top = "20%";
     input.style.left = "50%";
     input.style.transform = "translate(-50%, -50%)";
     input.style.border = "1px solid #000";
     parentContainer.appendChild(input);
     input.addEventListener("input", (e) => {
-        if (e.target.value.length === 4) {
+        if (e.target.value.length == 7) {
             getTaxi(e.target.value);
         }
     });
@@ -84,39 +93,50 @@ function placeInput() {
 }
 
 function getTaxi(texto) {
-    
-    let timeToReserve = texto.substring(0, 2) + ":" + texto.substring(2, 4) + ":00";
+    let timeToReserve = texto.split(":")[1] ? texto.split(":")[1] : time.getHours();
+    const captcha = texto.split(":")[0];
     let interval = setInterval(() => {
-    fetch("https://bus-med.1337.ma/create-reservation", {
-        "headers": {
-            Cookie: cookies,
-            "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
-            "accept-language": "en-US,en;q=0.9,zgh-MA;q=0.8,zgh;q=0.7,ar-MA;q=0.6,ar;q=0.5,fr-FR;q=0.4,fr;q=0.3",
-            "cache-control": "max-age=0",
-            "content-type": "application/x-www-form-urlencoded",
-            "sec-ch-ua": "\"Not/A)Brand\";v=\"99\", \"Google Chrome\";v=\"115\", \"Chromium\";v=\"115\"",
-            "sec-ch-ua-mobile": "?0",
-            "sec-ch-ua-platform": "\"macOS\"",
-            "sec-fetch-dest": "document",
-            "sec-fetch-mode": "navigate",
-            "sec-fetch-site": "same-origin",
-            "sec-fetch-user": "?1",
-            "upgrade-insecure-requests": "1"
-        },
-        "referrer": "https://bus-med.1337.ma/",
-        "referrerPolicy": "same-origin",
-        "body": `csrfmiddlewaretoken=${csrfmiddlewaretoken}&traget_id=50&captcha_0=${captcha_0}&captcha_1=${texto}`,
-        "method": "POST",
-        "mode": "cors",
-        "credentials": "include"
-    }).then(res => res.text()).then(console.log(`Done ✅ \n csrfmiddlewaretoken=${csrfmiddlewaretoken}&traget_id=50&captcha_0=${captcha_0}&captcha_1=${texto}`)).catch(err => console.error("Error: ", err));
-    if(timeToReserve == time )
-    {
-        clearInterval(interval);
-    }
+        if (timeToReserve == time.getHours()) {
+            fetch("https://bus-med.1337.ma/create-reservation", {
+                "headers": {
+
+                    "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+                    "accept-language": "en-US,en;q=0.9,zgh-MA;q=0.8,zgh;q=0.7,ar-MA;q=0.6,ar;q=0.5,fr-FR;q=0.4,fr;q=0.3",
+                    "cache-control": "max-age=0",
+                    "content-type": "application/x-www-form-urlencoded",
+                    "sec-ch-ua": "\"Not/A)Brand\";v=\"99\", \"Google Chrome\";v=\"115\", \"Chromium\";v=\"115\"",
+                    "sec-ch-ua-mobile": "?0",
+                    "sec-ch-ua-platform": "\"macOS\"",
+                    "sec-fetch-dest": "document",
+                    "sec-fetch-mode": "navigate",
+                    "sec-fetch-site": "same-origin",
+                    "sec-fetch-user": "?1",
+                    "upgrade-insecure-requests": "1"
+                },
+                "referrer": "https://bus-med.1337.ma/",
+                "referrerPolicy": "same-origin",
+                "body": `csrfmiddlewaretoken=${csrfmiddlewaretoken}&traget_id=${timeId[timeToReserve]}&captcha_0=${captcha_0}&captcha_1=${captcha}`,
+                "method": "POST",
+                "mode": "cors",
+                "credentials": "include"
+            }).then(res => res.text()).then(console.log(`Done ✅ `)).catch(err => console.error("Error: ", err));
+
+            clearInterval(interval);
+        }
     }, 10);
 }
 
 placeImage();
-// getElementsByName("csrfmiddlewaretoken")[0].value;
-// "body": `csrfmiddlewaretoken=${csrfmiddlewaretoken}&traget_id=50&captcha_0=${captcha_0}&captcha_1=${texto}`,
+
+
+// 11 AM = 9 id
+// 12 AM = 21
+// 13 = 52
+// 14 = 50
+// 16 = 30;
+// 18 = 33;
+// 19 = 40;
+// 20 = 41;
+// 21 = 29
+// 22 = 17
+// 23 = 38
